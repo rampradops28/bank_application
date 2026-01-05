b3da421a77fcd1c59b77939c8df112b9d36b2d9a
rampradops28 <rampradop28@gmail.com>
2026-01-05 06:26:47 +0530

    code updated

--- src/test/java/org/ram/GreetingResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.ram;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
-    // Execute the same tests but in packaged mode.
-}
